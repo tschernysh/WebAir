a3fc449e3f88ddd015442ac1bdbdf9581dd8cbc4
Umanskyi Alexander <44087027+Dampiere@users.noreply.github.com>
2021-02-22 00:19:31 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello u fucking trash
+    </div>
+  );
+}
+
+export default App;
