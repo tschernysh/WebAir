c0d3fab92ad4723191091e65d8509d38bdeec7cd
Umanskyi Alexander <44087027+Dampiere@users.noreply.github.com>
2021-02-23 17:37:27 +0200

    create Offer blocks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Offers from "./components/Offers/Offers";
 
 function App() {
   return (
     <div className="App">
-      Hello u fucking trash
+      <Offers/>
     </div>
   );
 }
