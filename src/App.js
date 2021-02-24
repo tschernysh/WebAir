b65d8064d137fa74104a17378516d156a4f57ebc
Umanskyi Alexander <44087027+Dampiere@users.noreply.github.com>
2021-02-24 18:18:22 +0200

    create Nowadays blocks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Offers from "./components/Offers/Offers";
 import Reviews from "./components/Reviews/Reviews";
+import Nowadays from "./components/Nowadays/Nowadays";
 
 function App() {
   return (
     <div className="App">
       <Offers/>
+      <Nowadays/>
       <Reviews/>
+
     </div>
   );
 }
