3652773522916a42cb016ef961f10f01ebab609b
Umanskyi Alexander <44087027+Dampiere@users.noreply.github.com>
2021-02-24 14:03:58 +0200

    create Reviews blocks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Offers from "./components/Offers/Offers";
+import Reviews from "./components/Reviews/Reviews";
 
 function App() {
   return (
     <div className="App">
       <Offers/>
+      <Reviews/>
     </div>
   );
 }
